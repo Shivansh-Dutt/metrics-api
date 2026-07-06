4055e97d3ad73d6adb7bf22e27619e5105e6922c
Shivansh Dutt <shivanshs497@gmail.com>
2026-07-06 14:27:03 +0530

    Token verification service

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+from fastapi.responses import JSONResponse
 import time
 import uuid
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import jwt
 
 app = FastAPI()
+
+EMAIL = "24f2002771@ds.study.iitm.ac.in"
+PUBLIC_KEY = """
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----
+"""
+
+ISSUER = "https://idp.exam.local"
+
+AUDIENCE = "tds-ppeartks.apps.exam.local"
+
+class TokenRequest(BaseModel):
+    token: str
 
 class MetricsMiddleware(BaseHTTPMiddleware):
 
@@ -33,8 +56,6 @@ app.add_middleware(
 app.add_middleware(MetricsMiddleware)
 
 
-EMAIL = "24f2002771@ds.study.iitm.ac.in"
-
 
 @app.get("/")
 def home():
@@ -58,3 +79,31 @@ def stats(values: str):
         "max": maximum,
         "mean": mean
     }
+    
+
+@app.post("/verify")
+def verify(request: TokenRequest):
+
+    try:
+        payload = jwt.decode(
+            request.token,
+            PUBLIC_KEY,
+            algorithms=["RS256"],
+            issuer=ISSUER,
+            audience=AUDIENCE,
+        )
+
+        return {
+            "valid": True,
+            "email": payload["email"],
+            "sub": payload["sub"],
+            "aud": payload["aud"],
+        }
+
+    except jwt.InvalidTokenError:
+        return JSONResponse(
+            status_code=401,
+            content={
+                "valid": False
+        }
+    )
